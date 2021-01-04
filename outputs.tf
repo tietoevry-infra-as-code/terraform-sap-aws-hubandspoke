17b5b2dc352b190d025e08456ec69d369e94786c
harshaly-gitrepo <61864262+harshaly-gitrepo@users.noreply.github.com>
2021-01-04 14:57:09 +0530

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,1 @@
+output "PUBLIC_IP" { value = aws_instance.test-tgw-instance3-shared.public_ip }
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "access_key" {
+  default = "xxxxxxxxxxxxxxxxx"
+}
+
+variable "secret_key" {
+  default = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "az1" {
+  default = "us-west-2a"
+}
+
+variable "az2" {
+  default = "us-west-2b"
+}
+
+variable "scenario" {
+  default = "test-tgw"
+}
+
+variable "public_key" {
+  default = "C:/Users/kholevij/Downloads/21Dec2020.pem"
+}
